82960d24b4ee93271694bc5168e787002ae7cb71
MahmoudAlsayes <mahmoudalsayes03@gmail.com>
2023-09-24 00:58:29 +0300

    chaneElhadeth

--- lib/tabs/ahadeth_tab.dart
+++ lib/tabs/ahadeth_tab.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islami/hadeth_model.dart';
+import 'package:islami/myThemeData.dart';
 
 class AhadethTab extends StatelessWidget {
-  const AhadethTab({super.key});
+  List<HadethModel> allAhadeth = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amberAccent,
+    loadHadeth();
+    return Column(
+      children: [
+        Center(
+          child: Image.asset("assets/images/ahadeth_image.png"),
+        ),
+        Divider(
+          thickness: 3,
+          color: MyThemeData.primaryColor,
+        ),
+        Text(
+          "Ahadeth",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        Divider(
+          height: 2,
+          thickness: 2,
+          color: MyThemeData.primaryColor,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Text(allAhadeth[index].title);
+            },
+            itemCount: allAhadeth.length,
+          ),
+        )
+      ],
+    );
+  }
+
+  void loadHadeth() {
+    rootBundle.loadString("assets/files/ahadeth.txt").then(
+      (ahadeth) {
+        List<String> ahadethList = ahadeth.split("#");
+
+        for (int i = 0; i < ahadethList.length; i++) {
+          String HadethOne = ahadethList[i];
+          List<String> hadethOneLines = HadethOne.trim().split("\n");
+          String title = hadethOneLines[0];
+          hadethOneLines.removeAt(0);
+          List<String> content = hadethOneLines;
+          HadethModel hadethModel = HadethModel(title, content);
+          print(hadethModel.title);
+          print("$i");
+          allAhadeth.add(hadethModel);
+        }
+      },
+    ).catchError(
+      (e) {
+        print(e.toString());
+      },
     );
   }
 }
